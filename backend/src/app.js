fd30891ec11a937ddc8706a892b2ad9f77404436
nmsoikat <nursaykat@gmail.com>
2022-11-01 02:07:19 +0600

    more security and not-found route

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xssClean = require('xss-clean')
+const hpp = require('hpp')
 
 const app = express()
 
 const { routes } = require('./api')
 const AppErrorHandler = require('./utils/app-error-handler')
+const { AppError } = require('./utils/app-error')
 
 //Global Middleware
 app.use(helmet())//Set Security HTTP headers
+app.use(mongoSanitize())// Input Data Sanitization against NoSQL Query Injection
+app.use(xssClean())// Input Data Sanitization against XSS //clean malicious html or script in html code //add before any route
+app.use(hpp({
+  //allow some duplicate query-params //if need
+  whitelist: ['duration']
+})) //Http Parameter Pollution // remove duplicate query param // ?name=sdf&sort=1&sort=2
+
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'development') {
@@ -18,6 +29,10 @@ if (process.env.NODE_ENV === 'development') {
 //Routes
 app.use('/api/v1/auth', routes.authRoute)
 
+//Not Found Route //all http verbs and // any route *
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can not find ${req.originalUrl} on this server`, 404))
+})
 
 app.use(AppErrorHandler)
 module.exports = app;
