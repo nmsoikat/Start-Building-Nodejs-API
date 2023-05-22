7f5508daaa6fbf22878d6f4a89576dc0437432a1
nmsoikat <nursaykat@gmail.com>
2023-05-22 23:18:39 +0600

    init pg and sequlize

---
+++ backend/src/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    host: 'localhost',
+    username: 'postgres',
+    password: '123456',
+    database: 'build_api',
+    dialect: 'postgres'
+  },
+  test: {
+    host: 'localhost',
+    username: 'postgres',
+    password: '123456',
+    database: 'build_api',
+    dialect: 'postgres'
+  },
+  production: {
+    host: 'localhost',
+    username: 'postgres',
+    password: '123456',
+    database: 'build_api',
+    dialect: 'postgres'
+  }
+}
